5a7f084a0c8b0b920608dd6931b169c7b732dabf
Pathum312 <pathum.2019557@iit.ac.lk>
2021-05-14 22:20:42 +0530

    updated to v1.3

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-gggggg = 'Pathum'
-print('Hello ' + gggggg + '!')
+id = [0, 1, 2, 3]
+name = ['Pathum', 'Johan', 'Tharaka', 'Rehan']
+
+count = 0
+
+print("id name")
+while (count < len(name)):
+    print("{}".format(id[count]) + "  " + name[count])
+    count = count + 1
