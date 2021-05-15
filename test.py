3370b774082d0d044227d4449674a0a79df89f60
Pathum312 <pathum.2019557@iit.ac.lk>
2021-05-15 10:32:53 +0530

    updated v1.6

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
+from texttable import Texttable
+
 id = [0, 1, 2, 3]
-name = ['Pathum', 'Johan', 'Tharaka', 'Rehan']
+name = ["Pathum", "Johan", "Tharaka", "Rehan"]
 
 count = 0
 
-print("id name")
-while (count < len(name)):
-    print("{}".format(id[count]) + "  " +name[count])
+t = Texttable()
+t.add_row(["id", "Name"])
+# print("id name")
+while count < len(name):
+    t.add_row([id[count], name[count]])
+    # print("{}".format(id[count]) + "  " + name[count])
     count = count + 1
+
+print(t.draw())
